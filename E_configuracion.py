86ae85df69148498ed340ff8c5fc81d3b2bde06a
lriccombene <lriccombene@gmail.com>
2019-08-04 19:14:43 -0300

    Update E_configuracion.py

--- E_configuracion.py
+++ E_configuracion.py
@@ -1,7 +1,27 @@
+'''
+Este archivo contiene metodos y variables de configuracion .
+This file contains methods and configuration variables.
+
+
+'''
+
+
+
+
+
+
 class configuracion(object):
+
+    '''
+    en este medtodo se debe armarn el string de coccion tipo de base, usuario, 
+    clave , nombre db y puerto de comunicacion del servicio de la db
+
+    In this method, the cooking string of the base type, 
+    user, password, db name and communication port of the db service must be set
+    '''
     def config(self):
         cadena = 'postgresql://postgres:slam2016@localhost:5432/bikeRental'
-        
+
         return cadena
 
     # lugar donde se descargan archivos local
